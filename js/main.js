c2fe7955a3d4c7e01d5a876833d5d1812b16a246
Miguel Rios <miguel@adianta.com.br>
2020-05-03 02:59:23 -0300

    feat(alive): Its alive!

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // executes when HTML-Document is loaded and DOM is ready
-    $('.activities-list div').each((i, element) => {
-        let words = element.innerText.split(' ');
-        element.innerText = words[0].toUpperCase() + ' ' + words.slice(1);
-    });
-});
