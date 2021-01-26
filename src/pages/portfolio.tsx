58014894afac6921157fd6731d66a06b54213b9e
Miguel Rios <miguelriosoliveira@gmail.com>
2021-01-26 17:03:07 -0300

    Added github_explorer to projects

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,9 +1,31 @@
 import Head from 'next/head';
 
+import githubExplorerImg from '../assets/github_explorer.png';
 import projectNetflixImg from '../assets/project_netflix.png';
 import projectPokedexImg from '../assets/project_pokedex.png';
 import ProjectCard from '../components/ProjectCard';
 import { Container } from '../styles/pages/Portfolio';
+
+const PROJECTS = [
+	{
+		name: 'Project Pokédex',
+		imgSrc: projectPokedexImg,
+		link: 'https://project-pokedex.vercel.app',
+		techs: ['ReactJS', 'Node.js', 'MongoDB', 'PokéAPI'],
+	},
+	{
+		name: 'Project Netflix',
+		imgSrc: projectNetflixImg,
+		link: 'https://project-netflix.herokuapp.com',
+		techs: ['ReactJS', 'TypeScript', 'TMDb API'],
+	},
+	{
+		name: 'github_explorer',
+		imgSrc: githubExplorerImg,
+		link: 'https://github-explorer-ts.vercel.app',
+		techs: ['ReactJS', 'TypeScript', 'GitHub API'],
+	},
+];
 
 const Portfolio: React.FC = () => {
 	return (
@@ -14,32 +36,11 @@ const Portfolio: React.FC = () => {
 
 			<Container>
 				<ul>
-					<li>
-						<ProjectCard
-							name="Project Pokédex"
-							imgSrc={projectPokedexImg}
-							link="https://project-pokedex.vercel.app/"
-							techs={['ReactJS', 'Node.js', 'MongoDB', 'PokéAPI']}
-						/>
-					</li>
-
-					<li>
-						<ProjectCard
-							name="Project Netflix"
-							imgSrc={projectNetflixImg}
-							link="https://project-netflix.herokuapp.com/"
-							techs={['ReactJS', 'TypeScript', 'TMDb API']}
-						/>
-					</li>
-
-					{/* <li>
-						<ProjectCard
-							name="github_explorer"
-							imgSrc={githubExplorerImg}
-							link="https://project-netflix.herokuapp.com/"
-							techs={['ReactJS', 'The Movie Database API']}
-						/>
-					</li> */}
+					{PROJECTS.map(project => (
+						<li key={project.name}>
+							<ProjectCard {...project} />
+						</li>
+					))}
 				</ul>
 			</Container>
 		</>
