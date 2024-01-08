c9ad23303a397b0133e69c723c786c489346c3fb
Miguel Rios <miguelriosoliveira@gmail.com>
2024-01-08 13:02:13 +0100

    fix: Commented usage of local fixtures

--- src/services/github.ts
+++ src/services/github.ts
@@ -13,10 +13,10 @@ const client = axios.create({ baseURL: 'https://api.github.com' });
 export const githubApi = {
 	async getRepoDetails(username: string, repo: string) {
 		try {
-			// const { data: repoDetails } = await client.get<Repository>(`/repos/${username}/${repo}`);
-			const repoDetails: Repository = JSON.parse(
-				await fs.readFile(`${process.cwd()}/src/fixtures/${repo}.json`, 'utf-8'),
-			);
+			const { data: repoDetails } = await client.get<Repository>(`/repos/${username}/${repo}`);
+			// const repoDetails: Repository = JSON.parse(
+			// 	await fs.readFile(`${process.cwd()}/src/fixtures/${repo}.json`, 'utf-8'),
+			// );
 			return repoDetails;
 		} catch (error) {
 			console.log('Failed getting repo details:', error);
